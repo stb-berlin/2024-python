7e32ebaa748f5bf304e8ccb2b77836eb0597bf7c
NetAcad DEVASC <changeme@example.com>
2024-11-14 15:14:52 +0000

    NONE: FIX - make possible to enter the VLAN values

--- devnet-src/python/if-vlan.py
+++ devnet-src/python/if-vlan.py
@@ -1,6 +1,6 @@
-nativeVLAN = 1
-dataVLAN = 100
+nativeVLAN = input("enter the number of the native VLAN: ")
+dataVLAN = input("enter the number of the data VLAN: ")
 if nativeVLAN == dataVLAN:
-    print(f"The native and data VLAN's are the same: {dataVLAN}")
+    print(f"The native and data VLAN's are the same: '{dataVLAN}'")
 else:
-    print(f"The native {nativeVLAN} and data {dataVLAN} VLAN's are different")
+    print(f"The native '{nativeVLAN}' and data '{dataVLAN}' VLAN's are different")
