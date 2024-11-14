71798fb19d1b5d1bac6b5c2ed17f3d9f894e406b
NetAcad DEVASC <changeme@example.com>
2024-11-14 15:48:52 +0000

    NONE: ADD - put routers in a separate list

--- devnet-src/python/for-devices.py
+++ devnet-src/python/for-devices.py
@@ -1,8 +1,13 @@
-devices=["R1","R2","R3","S1","S2"]
-switches=[]
+devices = ["R1","R2","R3","S1","S2"]
+switches = []
+routers = []
 for item in devices:
     if "S" in item:
         switches.append(item)
         print(item)
+    elif "R" in item:
+        routers.append(item)
+        print(item)
 
 print(f"my switches are: {switches}")
+print(f"my switches are: {routers}")
