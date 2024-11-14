800a99afff9a75f8d702eaf9a11272bf85298651
NetAcad DEVASC <changeme@example.com>
2024-11-14 15:44:11 +0000

    NONE: ADD - put switches in a separate list

--- devnet-src/python/for-devices.py
+++ devnet-src/python/for-devices.py
@@ -1,4 +1,8 @@
 devices=["R1","R2","R3","S1","S2"]
+switches=[]
 for item in devices:
-    if "R" in item:
+    if "S" in item:
+        switches.append(item)
         print(item)
+
+print(f"my switches are: {switches}")
