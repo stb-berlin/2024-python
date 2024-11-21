ac481602154327be04402a5e214f0a65ce90de9a
NetAcad DEVASC <changeme@example.com>
2024-11-21 15:13:30 +0000

    NONE: ADD - added endless loop to execute the script multiple times

--- devnet-src/python/while-loop.py
+++ devnet-src/python/while-loop.py
@@ -1,7 +1,11 @@
-x=int(input("Enter a number to count to: "))
-y=1
 while True:
-    print(y)
-    y=y+1
-    if y>x:
+    x=input("Enter a number to count to or 'q' to quit: ")
+    if x == 'q' or x == 'quit':
         break
+    x = int(x)
+    y=1
+    while True:
+        print(y)
+        y=y+1
+        if y>x:
+            break
