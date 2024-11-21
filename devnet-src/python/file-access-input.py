5e39fe4dc07aa3d43fa84a7ce13165a2b5c4bac6
NetAcad DEVASC <changeme@example.com>
2024-11-21 15:57:52 +0000

    NONE: ADD - add a new network device into the device list file

---
+++ devnet-src/python/file-access-input.py
@@ -0,0 +1,12 @@
+devices = []
+while True:
+    new_item = input("Enter device name or type 'exit' to quit: ")
+    if new_item == 'exit':
+        break
+
+    print("work in progress")
+    # alternate solution using "with"
+    with open("devices.txt","r") as myfile:
+        for item in myfile:
+            devices.append(item.strip())
+    print(f"total: {len(devices)} of {devices}")
