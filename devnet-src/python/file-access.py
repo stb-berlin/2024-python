7fc3cc253d577e09e07f9b58bc82a368d0d84f40
NetAcad DEVASC <changeme@example.com>
2024-11-21 15:31:55 +0000

    NONE: ADD - save file's lines in a list

--- devnet-src/python/file-access.py
+++ devnet-src/python/file-access.py
@@ -1,4 +1,12 @@
+devices = []
 myfile = open("devices.txt","r")
 for item in myfile:
-    print(item.strip())
-file.close()
+    devices.append(item.strip())
+myfile.close()
+print(f"total: {len(devices)} of {devices}")
+
+# alternate solution using "with"
+with open("new-devices.txt","a+") as myfile:
+    for item in myfile:
+        devices.append(item.strip())
+print(f"total: {len(devices)} of {devices}")
